33b3cc322d4d05e19e3c7e1370ccbe1ab0c4143e
Ashley Xing <57583443+uncleroach@users.noreply.github.com>
2020-02-08 13:53:40 -0500

    Add files via upload

--- app.js
+++ app.js
@@ -23,18 +23,92 @@ app.use(
 // ------------------------------------------------------------------
 // APP LOGIC
 // ------------------------------------------------------------------
+var balance = 0;
+var GPA = 4.0;
+var happiness = 7;
+var year = 1;
+var eventID = [];
+const calculateBalance = function(){
 
+
+}
 app.setHandler({
     LAUNCH() {
-        return this.toIntent('HelloWorldIntent');
+        console.log("launched");
+        let background = 'You’re a senior in high school, trying to decide what to do for next year. You live in a middle-class family, with a household income of 65,000 dollars. You’ve been accepted to a good state school, and your tuition is 20,000 dollars a year. Your parents can’t contribute to this tuition. You have the option to either get a bachelor’s degree, or to get a job in the workforce. If you get a job, you’ll make 30,000 dollars a year. Will you go to college or get a job?';
+        let repeat = 'Will you go to college or get a job?';
+        this.ask(background,repeat);
+    },
+    CollegeIntent(){
+        console.log("went to college");
+        let college = 'Now that you’ve decided to pursue higher education, you need to take out a federal loan of 20,000 dollars, which has a fixed interest rate of 5% a year. You could also participate in a work-study program and earn up to 2,500 dollars a year. Would you like to do work-study?';
+        let repeat = 'Would you like to do work-study?';
+        console.log("before this.ask");
+        this.followUpState('WorkStudyState').ask(college,repeat);
+        console.log("after this.ask");
+      //  this.toIntent('RandomEventIntent');
+    },
+    WorkStudyState: {
+        YesIntent(){
+            console.log("yes");
+            this.toIntent('RandomEventIntent');
+        },
+        NoIntent(){
+            console.log("no");
+            this.toIntent('RandomEventIntent');
+        },
+    },
+    RandomEventIntent(){
+        var id = Math.floor((Math.random()*4)+1);
+        while (eventID.indexOf(id)<0){
+            id = Math.floor((Math.random()*4)+1);
+        }
+        elementID.push(id);
+        if (id==1){
+            this.toIntent('PartyIntent');
+        }
+        else if (id==2){
+            this.toIntent('ConcertIntent');
+        }
+        else if (id==3){
+            this.toIntent('BorrowIntent');
+        }
+        else {
+            this.toIntent('WalletIntent');
+        }
+    },
+    PartyIntent(){
+        console.log("party intent");
+    },
+    ConcertIntent(){
+        console.log("console intent");
+    },
+    BorrowIntent(){
+        console.log("borrow intent");
+    },
+    WalletIntent(){
+        console.log("wallet intent");
     },
 
-    HelloWorldIntent() {
-        this.ask('Hello World! What\'s your name?', 'Please tell me your name.');
-    },
 
-    MyNameIsIntent() {
-        this.tell('Hey ' + this.$inputs.name.value + ', nice to meet you!');
+
+    GapYearIntent(){
+        
+    },
+    BalanceIntent(){
+
+    },
+    HappinessIntent(){
+
+    },
+    GPAIntent(){
+
+    },
+    SituationIntent(){
+
+    },
+    LoseIntent(){
+
     },
 });
 
